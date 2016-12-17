14994c1a6178ef1dc3db3318a801914c885edc70
wenbobao <744865006@qq.com>
2016-12-17 14:43:26 +0800

    add IntermediateiOS10Programming

--- HackingWithSwift/Project1_1/Project1_1/DetailViewController.swift
+++ HackingWithSwift/Project1_1/Project1_1/DetailViewController.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    @IBOutlet weak var detailImageView: UIImageView!
+    
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
@@ -23,8 +22,8 @@ class DetailViewController: UIViewController {
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail as! String)
             }
         }
     }
@@ -33,6 +32,16 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Swift学学习笔记.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+//as!  强制类型转换,如果转换失败会报 runtime 运行错误
+//as?  as? 如果转换不成功的时候便会返回一个 nil 对象。成功的话返回可选类型值（optional），需要我们拆包使用。
